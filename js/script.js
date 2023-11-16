d824ad5fe4a87f9630e901082a4372af3e148afa
jriveradepaz <jonathanrivera011@icloud.com>
2023-11-15 20:20:59 -0600

    commit 11
    attempted to fix map not appearing and changed name of map page on all my other htmls

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-function init(){
+function initMap(){
 	var map = new google.maps.Map(document.getElementById('map'),{
 	center: { lat: -34.397, lng: 150.644 },
-    zoom: 4;
+    zoom: 4
   });
   
-
-initMap();
 }
 
 google.maps.event.addDomListener(window, 'load', init);
