8bcba96732708045e9d8c7817dda484367a5d409
jriveradepaz <jonathanrivera011@icloud.com>
2023-12-04 11:35:28 -0600

    commit 14
    Attempted to fix the map not loading

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-function initMap(){
-	var map = new google.maps.Map(document.getElementById('map'),{
-	center: { lat: -34.397, lng: 150.644 },
-    zoom: 4
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+     const center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
-  
 }
 
-google.maps.event.addDomListener(window, 'load', init);
+initMap();
