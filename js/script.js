dcad4650de027e308992922bbd6614ab0c5f8850
jriveradepaz <jonathanrivera011@icloud.com>
2023-12-04 20:44:08 -0600

    commit 29
    attempt to fix map

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
+function init(){
+	var el = document.getElementById('map');
+	var myLocation = new google.maps.LatLng(41.882629, -87.623474);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.SATELLITE,
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
+		}
+	};
+
+	var myMap = new google.maps.Map(el, mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		
+	});
+
+	var contentString = '<h1>millenium park</h1><p>Attractions: Giant Bean, Ice skating ring, and Christmas tree</p>';
+
+	var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	});
+
+	google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
 
 
-function initMap(){
-  const { Map } = await google.maps.importLibrary("maps");
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 41.8826, lng: 87.6226},
-      zoom: 4
-    });
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+google.maps.event.addDomListener(window, 'load', init);
