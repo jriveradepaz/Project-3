6b6509c020df43e2057edc828f258878c952fcd6
jriveradepaz <jonathanrivera011@icloud.com>
2023-12-04 19:29:15 -0600

    commit 20
    checking if the api key has been enabled

--- js/script.js
+++ js/script.js
@@ -1,31 +1,10 @@
-const apiKey = AIzaSyDJlDF23SK4JzimLJQo65rH9jDY53EjhqY;
-// Initialize and add the map
-let map;
 
-async function initMap() {
-
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
- 
-
- // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
+var map; 
+function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
 }
 
-initMap();
+google.maps.event.addDomListener(window, 'load', initMap);
