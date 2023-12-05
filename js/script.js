41a2b8de2a1cdf965bc23c7a82fd4c0379444cef
jriveradepaz <jonathanrivera011@icloud.com>
2023-12-04 19:47:12 -0600

    commit 23
    add import library in an attempt to fix an error

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 var map; 
 function initMap(){
+  const { Map } = await google.maps.importLibrary("maps");
   var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 41.8826, lng: 87.6226},
       zoom: 4
